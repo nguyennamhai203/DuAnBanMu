fb588f8649e204e1aeac890d4e6f681210d5bba1
Nam Hai <hainnph27584@fpt.edu.vn>
2024-03-31 18:13:46 +0700

    fix gia ban khi khuyenmai phan tram

--- Shop_Api/Services/ProductService.cs
+++ Shop_Api/Services/ProductService.cs
@@ -92,7 +92,7 @@ namespace Shop_Api.Services
                             // Cập nhật giá sản phẩm dựa trên loại hình khuyến mãi
                             if (currentPromotion.LoaiHinhKhuyenMai == "Khuyến mại giảm giá")
                             {
-                                product.GiaBan = product.GiaNhap - (product.GiaNhap * currentPromotion.MucGiam / 100);
+                                product.GiaBan = product.GiaBan - (product.GiaBan * currentPromotion.MucGiam / 100);
                             }
                             else if (currentPromotion.LoaiHinhKhuyenMai == "Khuyến mại đồng giá")
                             {
